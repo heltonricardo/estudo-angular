7bbcffe5958a9568c4568c9869d2e4ff9d98bf9f
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-11-03 13:27:07 -0300

    enviando dados para a view e iterando com o for

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
 })
 export class CursosComponent implements OnInit {
+  nomePortal: string;
+  cursos: string[] = ['Java', 'JavaScript', 'Angular'];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.nomePortal = 'udemy.com';
   }
 
+  ngOnInit(): void {}
 }
+ 
