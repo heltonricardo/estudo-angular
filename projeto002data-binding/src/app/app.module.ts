f54f7a61c690c40df3e97b14ce192a7950355f2d
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-11-04 17:24:43 -0300

    two way data binding

--- projeto002data-binding/src/app/app.module.ts
+++ projeto002data-binding/src/app/app.module.ts
@@ -1,18 +1,16 @@
+import { MeuFormModule } from './meu-form/meu-form.module';
+import { MeuFormComponent } from './meu-form/meu-form.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DataBindingComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DataBindingComponent],
+  imports: [BrowserModule, FormsModule, MeuFormModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
